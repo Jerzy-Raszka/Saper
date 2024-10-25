82322be16636e4d1d5f0786f8fb3a62601b7d548
Jerzy Raszka <jerzyraszka2000@gmail.com>
2024-10-25 11:03:40 +0200

    Added revealing fields by user input

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@ class Field {
 public:
   bool isMine = false;
   bool isHidden = true;
-  int Number;
+  int Number = 0;
 };
 
 Field board[9][9];
@@ -14,30 +14,40 @@ Field board[9][9];
 int main() {
   int x, y;
 
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_int_distribution<> distrib(0, 8);
+
+  for (int i = 0; i < 10; i++) {
+    int j = distrib(gen);
+    int k = distrib(gen);
+    if (board[j][k].isMine == true) {
+
+      i--;
+
+    } else {
+      (board[j][k].isMine = true);
+    }
+  }
+
   do {
     printf("Podaj rząd oraz kolumne\n");
     scanf("%d %d", &x, &y);
-    printf("%d %d", x, y);
 
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::uniform_int_distribution<> distrib(0, 8);
-
-    for (int i = 0; i < 10; i++) {
-      int j = distrib(gen);
-      int k = distrib(gen);
-      if (board[j][k].isMine == true) {
-
-        i--;
-
-      } else {
-        (board[j][k].isMine = true);
-      }
-    }
+    board[x][y].isHidden = false;
+    board[x][y].Number = 1;
 
     for (int i = 0; i < 9; i++) {
       for (int j = 0; j < 9; j++) {
-        std::cout << "#";
+        if (board[i][j].isHidden == false) {
+          if (board[i][j].isMine == false) {
+            std::cout << board[i][j].Number;
+          } else {
+            std::cout << "*";
+          }
+        } else {
+          std::cout << "#";
+        }
       }
       std::cout << "\n";
     }
