53998588f2c8bff2ba6fb44b42e34ce7cee3818c
Jerzy Raszka <jerzyraszka2000@gmail.com>
2024-10-26 13:29:24 +0200

    Added zeros revealing fields recursively

--- src/main.cpp
+++ src/main.cpp
@@ -11,8 +11,11 @@ public:
 
 Field board[9][9];
 
-int main() {
-  int x, y;
+bool isValid(int row, int col) {
+  return ((row >= 0) && (row < 9) && (col >= 0) && (col < 9));
+}
+
+void placeMines() {
 
   std::random_device rd;
   std::mt19937 gen(rd());
@@ -29,30 +32,88 @@ int main() {
       (board[j][k].isMine = true);
     }
   }
+}
 
-  do {
-    printf("Podaj rząd oraz kolumne\n");
-    scanf("%d %d", &x, &y);
-
-    board[x][y].isHidden = false;
-    board[x][y].Number = 1;
-
-    for (int i = 0; i < 9; i++) {
-      for (int j = 0; j < 9; j++) {
-        if (board[i][j].isHidden == false) {
-          if (board[i][j].isMine == false) {
-            std::cout << board[i][j].Number;
-          } else {
-            std::cout << "*";
-          }
+void printBoard() {
+  for (int i = 0; i < 9; i++) {
+    for (int j = 0; j < 9; j++) {
+      if (board[i][j].isHidden == false) {
+        if (board[i][j].isMine == false) {
+          std::cout << board[i][j].Number;
         } else {
-          std::cout << "#";
+          std::cout << "*";
+        }
+      } else {
+        std::cout << "#";
+      }
+    }
+    std::cout << "\n";
+  }
+}
+
+void assignNumbers(int row, int col) {
+  int dx[8] = {-1, -1, -1, 0, 0, 1, 1, 1};
+  int dy[8] = {-1, 0, 1, -1, 1, -1, 0, 1};
+
+  for (int d = 0; d < 8; d++) {
+    int newRow = row + dx[d];
+    int newCol = col + dy[d];
+
+    if (isValid(newRow, newCol) == true) {
+      if (board[newRow][newCol].isMine == true) {
+        board[row][col].Number++;
+      }
+    }
+  }
+}
+
+void showZeros(int row, int col) {
+  if (board[row][col].isHidden == true) {
+    board[row][col].isHidden = false;
+    if (board[row][col].Number == 0) {
+      int dx[8] = {-1, -1, -1, 0, 0, 1, 1, 1};
+      int dy[8] = {-1, 0, 1, -1, 1, -1, 0, 1};
+
+      for (int d = 0; d < 8; d++) {
+        int newRow = row + dx[d];
+        int newCol = col + dy[d];
+
+        if (isValid(newRow, newCol) == true) {
+
+          if (board[newRow][newCol].Number == 0) {
+            showZeros(newRow, newCol);
+          }
+          board[newRow][newCol].isHidden = false;
         }
       }
-      std::cout << "\n";
     }
+  }
+}
+void makeMove() {
+  int x, y;
+  printf("Podaj rząd oraz kolumne\n");
+  scanf("%d %d", &x, &y);
+  showZeros(x, y);
+}
 
-  } while (board[x][y].isMine != true);
+int main() {
+
+  placeMines();
+  for (int i = 0; i < 9; i++) {
+    for (int j = 0; j < 9; j++) {
+      assignNumbers(i, j);
+    }
+  }
+  printBoard();
+
+  makeMove();
+  printBoard();
+  makeMove();
+  printBoard();
+  makeMove();
+  printBoard();
+  makeMove();
+  printBoard();
 
   return 0;
 }
